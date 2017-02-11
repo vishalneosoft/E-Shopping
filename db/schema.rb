7d07bf9ec48921805c7cb80cc7684f52f2648fa3
Webwerks1 <webwerks1@Z3R3P9.(none)>
2017-02-11 15:42:30 +0530

    added wishlist scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170209112607) do
+ActiveRecord::Schema.define(version: 20170211094929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -170,6 +170,17 @@ ActiveRecord::Schema.define(version: 20170209112607) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  create_table "wish_lists", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "product_id"
+    t.integer  "quantity",   default: 1
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "wish_lists", ["product_id"], name: "index_wish_lists_on_product_id", using: :btree
+  add_index "wish_lists", ["user_id"], name: "index_wish_lists_on_user_id", using: :btree
+
   add_foreign_key "addresses", "users"
   add_foreign_key "brand_categories", "brands"
   add_foreign_key "brand_categories", "categories"
@@ -183,4 +194,6 @@ ActiveRecord::Schema.define(version: 20170209112607) do
   add_foreign_key "product_categories", "categories"
   add_foreign_key "product_categories", "products"
   add_foreign_key "products", "brands"
+  add_foreign_key "wish_lists", "products"
+  add_foreign_key "wish_lists", "users"
 end
