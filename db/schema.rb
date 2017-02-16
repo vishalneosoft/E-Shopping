f03f54f64c78f12bc33f55451a8699572c38493e
Webwerks1 <webwerks1@Z3R3P9.(none)>
2017-02-16 11:33:54 +0530

    Added contact field related products and similar products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214140342) do
+ActiveRecord::Schema.define(version: 20170215145434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,16 @@ ActiveRecord::Schema.define(version: 20170214140342) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "subject"
+    t.text     "message"
+    t.text     "reply"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "order_items", force: :cascade do |t|
     t.integer  "order_id"
     t.integer  "product_id"
@@ -146,6 +156,11 @@ ActiveRecord::Schema.define(version: 20170214140342) do
   end
 
   add_index "products", ["brand_id"], name: "index_products_on_brand_id", using: :btree
+
+  create_table "recommended_products", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "recommended_product_id"
+  end
 
   create_table "transactions", force: :cascade do |t|
     t.string   "charge"
